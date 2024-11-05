c89a7c8feef1828fe0e61b8c80e5f8816e5f1832
DESKTOP-RNVMINN\vim <vimcoders@gmail.com>
2024-11-05 22:48:35 +0800

    Net

--- Scripts/Net/TcpClient.cs
+++ Scripts/Net/TcpClient.cs
@@ -6,15 +6,16 @@ using System.Linq;
 using Google.Protobuf;
 using Pb;
 using System.Reflection;
-using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
+using System.Threading;
 
 public class Method
 {
-    public UInt16 Id { get; set;}
-    public String Name { get; set;}
-    public String Request { get; set;}
-    public String Response { get; set;}
+    public ushort Id { get; set;}
+    public string MethodName { get; set;}
+    public string RequestName { get; set;}
+    public string ResponseName { get; set;}
+    public IMessage Request { get; set;}
+    public IMessage Response { get; set;}
 }
 
 public class TcpClient : MonoBehaviour
@@ -23,115 +24,109 @@ public class TcpClient : MonoBehaviour
     IPAddress ip;
     IPEndPoint ipEnd;
     Method[] Methods;
-    byte[] buffer;
- 
-    void Send(IMessage m)
+    byte[] readBuffer;
+    byte[] writeBuffer;
+    int count;
+    Thread t;
+
+    void Send(IMessage msg)
     {
-        byte[] d = Encode(m);
-        this.s.Send(d, d.Length, SocketFlags.None);
+        byte[] payload = msg.ToByteArray();
+        ushort length = Convert.ToUInt16(4 + payload.Length);
+        Method method = Methods.Where(t => t.Request.Descriptor.Name == msg.Descriptor.Name).First();
+        writeBuffer[0] = (byte)((length >> 8) & 0xFF);
+        writeBuffer[1] = (byte)(length & 0xFF);
+        writeBuffer[2] = (byte)((method.Id >> 8) & 0xFF);
+        writeBuffer[3] = (byte)(method.Id & 0xFF);
+        Buffer.BlockCopy(payload, 0, writeBuffer, 4, payload.Length);
+        s.Send(writeBuffer, length, SocketFlags.None);
     }
 
-    /// <summary>
-    /// encode
-    /// </summary>
-    /// <param name="data"></param>
-    /// <returns></returns>
-    public byte[] Encode(IMessage msg) {
-        for (int i = 0; i < Methods.Length; i++) 
-        {
-            if (Methods[i].Request != msg.Descriptor.Name)
-            {
-                continue;
-            }
-            byte[] payload = msg.ToByteArray();
-            byte[] buffer = new byte[0];
-            ushort length = Convert.ToUInt16(4+payload.Length);
-            buffer = buffer.Concat(BitConverter.GetBytes(length).Reverse()).ToArray();
-            buffer = buffer.Concat(BitConverter.GetBytes(Methods[i].Id).Reverse()).ToArray();
-            return buffer.Concat(payload).ToArray();
-        }
-        return null;
-    }
-
-    /// <summary>
-    /// Decode
-    /// </summary>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public IMessage Decode(byte[] b) {
-        ushort cmd = BitConverter.ToUInt16(b.Skip(2).Take(2).ToArray());
-        if (cmd >= Methods.Length) {
-            return null;
-        }
-        var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "Pb");
-        foreach (var t in types)
-        {
-            if (t.Name != this.Methods[cmd].Response)
-            {
-                continue;
-            }
-            IMessage myInstance = (IMessage)Activator.CreateInstance(t);
-            byte[] payload = b.Skip(4).ToArray();
-            return myInstance.Descriptor.Parser.ParseFrom(payload);
-        }
-        return null;
-    }
- 
     void Close()
     {
+        s?.Shutdown(SocketShutdown.Both);
         s?.Close();
-        print("diconnect");
     }
- 
+
     // Use this for initialization
     void Start()
     {
+        readBuffer = new byte[1024];
+        writeBuffer = new byte[1024];
         Methods = new Method[]{new() {
             Id = 0,
-            Name = "Ping",
-            Request = "PingRequest",
-            Response = "PingResponse",
+            MethodName = "Ping",
+            RequestName = "PingRequest",
+            ResponseName = "PingResponse",
         }};
-        this.buffer = new byte[0];
+        var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "Pb");
+        for (int i = 0; i < Methods.Length; i++)
+        {
+            var request = types.Where(t => t.Name == Methods[i].RequestName).First();
+            var response = types.Where(t => t.Name == Methods[i].ResponseName).First();
+            Methods[i].Request = (IMessage)Activator.CreateInstance(request);
+            Methods[i].Response = (IMessage)Activator.CreateInstance(response);
+        }
         ip = IPAddress.Parse("127.0.0.1");
         ipEnd = new IPEndPoint(ip, 9600);
         s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         s.Connect(ipEnd);
+        t = new Thread(new ThreadStart(Receive));
+        t.Start();
     }
- 
+
+    /// <summary>
+    /// Receive
+    /// </summary>
+    void Receive()
+    {
+        while (true)
+        {
+           receive();
+        }
+    }
+
+
+    /// <summary>
+    /// receive
+    /// </summary>
+    void receive()
+    {
+        int len = s.Receive(readBuffer, count, readBuffer.Length, SocketFlags.None);
+        if (len == 0)
+        {
+            return;
+        }
+        count += len;
+        while (count >= 4) {
+            ushort length = 0;
+            length |= readBuffer[0];
+            length <<= 8;
+            length |= readBuffer[1];
+            if (count < length) 
+            {
+                return;
+            }
+            ushort cmd = 0;
+            cmd |= readBuffer[2];
+            cmd <<= 8;
+            cmd |= readBuffer[3];
+            if (cmd >= Methods.Length) {
+                return;
+            }
+            IMessage iMessage = Methods[cmd].Response.Descriptor.Parser.ParseFrom(readBuffer, 4, length-4);
+            Buffer.BlockCopy(readBuffer, length, readBuffer, 0, length);
+            count -= length;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         var request = new PingRequest(){Message = ByteString.CopyFromUtf8("Hello")};
         Send(request);
-        byte[] b = new byte[1024];
-        int len = s.Receive(b);
-        if (len == 0)
-        {
-            return;
-        }
-        var newBuffer = buffer.Concat(b.Take(len)).ToArray();
-        for (int i = 0; i <= 1000; i++) {
-            ushort length = BitConverter.ToUInt16(newBuffer.Take(2).Reverse().ToArray());
-            if (length <= 0) 
-            {
-                return;
-            }
-            if (length > newBuffer.Length) {
-                this.buffer = newBuffer;
-                return;
-            }
-            IMessage iMessage = Decode(newBuffer.Take(length).ToArray());
-            Debug.Log("receive" + iMessage);
-            newBuffer = newBuffer.Skip(length).ToArray();
-            if (newBuffer.Length <= 2) 
-            {
-                this.buffer = newBuffer;
-                return;
-            }
-        }
     }
- 
+
     void OnApplicationQuit()
     {
         Close();
