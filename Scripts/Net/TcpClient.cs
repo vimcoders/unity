62daa64f13ea965c4986798fdc340daae6b95d8b
DESKTOP-RNVMINN\vim <vimcoders@gmail.com>
2024-11-06 20:06:08 +0800

    Net

--- Scripts/Net/TcpClient.cs
+++ Scripts/Net/TcpClient.cs
@@ -4,21 +4,43 @@ using System.Net.Sockets;
 using System;
 using System.Linq;
 using Google.Protobuf;
-using Pb;
 using System.Reflection;
 using System.Threading;
+using System.IO;
+using System.Text;
+using Pb;
 
-public delegate void OnMessage(IMessage msg);
+[System.Serializable]
+public class Response<T> {
+    public int code;
+    public string message;
+    public T data;
+}
 
+[System.Serializable]
+public class PassportLoginRequest {
+	public string Passport;
+	public string Pwd;      
+}
+
+[System.Serializable]
+public class PassportLoginResponse {
+	public string token;
+	public Method[] methods;
+}
+
+[System.Serializable]
 public class Method
 {
-    public ushort Id { get; set;}
-    public string MethodName { get; set;}
-    public string RequestName { get; set;}
-    public string ResponseName { get; set;}
-    public IMessage Request { get; set;}
-    public IMessage Response { get; set;}
+    public ushort Id;
+    public string MethodName;
+    public string RequestName;
+    public string ResponseName;
+    public IMessage Request;
+    public IMessage Response;
 }
+
+public delegate void OnMessage(IMessage msg);
 
 public class TcpClient : MonoBehaviour
 {
@@ -48,47 +70,9 @@ public class TcpClient : MonoBehaviour
 
     void Close()
     {
-        s?.Shutdown(SocketShutdown.Both);
         s?.Close();
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-        count = 0;
-        readBuffer = new byte[1024];
-        writeBuffer = new byte[1024];
-        Methods = new Method[]{new() {
-            Id = 0,
-            MethodName = "Ping",
-            RequestName = "PingRequest",
-            ResponseName = "PingResponse",
-        }};
-        var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "Pb");
-        for (int i = 0; i < Methods.Length; i++)
-        {
-            var request = types.Where(t => t.Name == Methods[i].RequestName).First();
-            var response = types.Where(t => t.Name == Methods[i].ResponseName).First();
-            Methods[i].Request = (IMessage)Activator.CreateInstance(request);
-            Methods[i].Response = (IMessage)Activator.CreateInstance(response);
-        }
-        ip = IPAddress.Parse("127.0.0.1");
-        ipEnd = new IPEndPoint(ip, 9600);
-        s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        s.Connect(ipEnd);
-        t = new Thread(new ThreadStart(StartReceive));
-        t.Start();
-    }
-
-    /// <summary>
-    /// StartReceive
-    /// </summary>
-    void StartReceive()
-    {
-        while (true)
-        {
-           Receive();
-        }
+        s?.Dispose();
+        t?.Join();
     }
 
     /// <summary>
@@ -116,6 +100,68 @@ public class TcpClient : MonoBehaviour
             OnMessage?.Invoke(iMessage);
             Buffer.BlockCopy(readBuffer, length, readBuffer, 0, length);
             count -= length;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        string url = "http://127.0.0.1:9800/api/v1/passport/login";
+        string loginRequest = JsonUtility.ToJson(new PassportLoginRequest() {
+            Passport = Guid.NewGuid().ToString(),
+            Pwd = Guid.NewGuid().ToString(),
+        });
+        byte[] loginRequestBytes = Encoding.UTF8.GetBytes(loginRequest);
+        var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+        httpRequest.Method = "POST";
+        httpRequest.ContentType = "application/json;charset=UTF-8";
+        httpRequest.ContentLength = loginRequestBytes.Length;
+        using Stream writer = httpRequest.GetRequestStream();
+        writer.Write(loginRequestBytes, 0, loginRequestBytes.Length);
+        var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+        using var reader = new StreamReader(httpResponse.GetResponseStream(), Encoding.UTF8);
+        string responseJson = reader.ReadToEnd();
+        var loginResponse = JsonUtility.FromJson<Response<PassportLoginResponse>>(responseJson);
+        var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "Pb");
+        this.Methods = new Method[0];
+        this.count = 0;
+        this.readBuffer = new byte[1024];
+        this.writeBuffer = new byte[1024];
+        for (int i = 0; i < loginResponse.data.methods.Length; i++)
+        {
+            var request = types.Where(t => t.Name == loginResponse.data.methods[i].RequestName).First();
+            var response = types.Where(t => t.Name == loginResponse.data.methods[i].ResponseName).First();
+            Methods = Methods.Append(new Method{
+                Id = loginResponse.data.methods[i].Id,
+                Request = (IMessage)Activator.CreateInstance(request),
+                Response = (IMessage)Activator.CreateInstance(response),
+            }).ToArray();
+        }
+        ip = IPAddress.Parse("127.0.0.1");
+        ipEnd = new IPEndPoint(ip, 9600);
+        s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        s.Connect(ipEnd);
+        t = new Thread(new ThreadStart(StartReceive));
+        t.Start();
+    }
+
+    /// <summary>
+    /// StartReceive
+    /// </summary>
+    void StartReceive()
+    {
+        while (true)
+        {
+            try
+            {
+                Receive();
+            }
+            catch (SocketException ex)
+            {
+                int err = ex.ErrorCode;
+                Debug.Log("StartReceive"+err.ToString());
+                return;
+            }
         }
     }
 
